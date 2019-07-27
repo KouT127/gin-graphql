7aa0eb23d227a895be56d1cafa5f0036f28e107c
KouT127 <kou26haru@gmail.com>
2019-07-27 23:25:04 +0900

    modify: add logger config

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,1 +1,16 @@
 package config
+
+import (
+	. "github.com/labstack/echo/middleware"
+	"os"
+)
+
+var (
+	DebugLoggerConfig = LoggerConfig{
+		Skipper: DefaultSkipper,
+		Format: `[${method}:${status}] ${uri} [${latency_human}] bytes_in:${bytes_in} bytes_out:${bytes_out}` + "\n",
+		CustomTimeFormat: "2006-01-02 15:04:05.00000",
+		Output:           os.Stdout,
+	}
+)
+
--- backend/server/router.go
+++ backend/server/router.go
@@ -2,11 +2,10 @@ package server
 
 import (
 	"github.com/99designs/gqlgen/handler"
+	. "github.com/KouT127/gin-sample/backend/config"
 	"github.com/KouT127/gin-sample/backend/interface/graphql"
 	"github.com/KouT127/gin-sample/backend/interface/graphql/generated"
 	"github.com/KouT127/gin-sample/backend/interface/middlewares/dataloader"
-	"github.com/KouT127/gin-sample/backend/interface/middlewares/security"
-	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -20,55 +19,23 @@ var db *gorm.DB
 //}
 
 func Init() {
-	e := echo.New()
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	e.Use(dataloader.LoaderMiddleware)
-	e.POST("/query", echo.WrapHandler(handler.GraphQL(generated.NewExecutableSchema(generated.Config{Resolvers: &graphql.Resolver{}}))))
-	e.GET("/", echo.WrapHandler(handler.Playground("GraphQL", "/query")))
+	e := NewRouter()
 	e.Start(":8080")
 }
 
-func NewRouter() *gin.Engine {
-	r := gin.New()
-	r.Use(gin.Logger())
-	r.Use(gin.Recovery())
-	r.Use(security.CORSMiddleware())
-	//r.Use(dataloader.LoaderMiddleware())
-
-	r.POST("/query", graphqlHandler())
-	r.GET("/", playgroundHandler())
-
-	v1 := r.Group("v1")
-	{
-		newUserHandler(v1)
-	}
-	return r
+func NewRouter() *echo.Echo {
+	e := echo.New()
+	e.Use(middleware.Recover())
+	e.Use(middleware.LoggerWithConfig(DebugLoggerConfig))
+	e.Use(dataloader.LoaderMiddleware)
+	e.POST("/query", graphqlHandler())
+	e.GET("/", playgroundHandler())
+	return e
 }
 
-func newUserHandler(gr *gin.RouterGroup) {
-	userGr := gr.Group("users")
-	{
-		uc := InjectUser()
-		userGr.GET("", uc.Get)
-		userGr.POST("", uc.Create)
-		//userGr.PUT(":id/", uc.Update)
-		//userGr.DELETE(":id/", uc.Delete)
-	}
+func graphqlHandler() echo.HandlerFunc {
+	return echo.WrapHandler(handler.GraphQL(generated.NewExecutableSchema(generated.Config{Resolvers: &graphql.Resolver{}})))
 }
-
-func graphqlHandler() gin.HandlerFunc {
-	h := handler.GraphQL(generated.NewExecutableSchema(generated.Config{Resolvers: &graphql.Resolver{}}))
-
-	return func(c *gin.Context) {
-		h.ServeHTTP(c.Writer, c.Request)
-	}
-}
-
-func playgroundHandler() gin.HandlerFunc {
-	h := handler.Playground("GraphQL", "/query")
-
-	return func(c *gin.Context) {
-		h.ServeHTTP(c.Writer, c.Request)
-	}
+func playgroundHandler() echo.HandlerFunc {
+	return echo.WrapHandler(handler.Playground("GraphQL", "/query"))
 }
